864808a931da536524a7fb1462b0b079bf39cf75
matYang <uwse@me.com>
2012-11-23 05:16:07 -0500

    POST testing modified

--- scripts/view/MessageView.js
+++ scripts/view/MessageView.js
@@ -16,7 +16,8 @@ var MessageView = Backbone.View.extend({
 		var newMessage = new Message();
 		var locationString = $('#provinceValue').val() + " " + $('#cityValue').val() + " " + $('#regionValue').val() + " " + $('#schoolValue').val();
 
-		newMessage.set({'userName' : $('#userNameValue').val(), 'password' : $('#passwordValue').val(), 'date' : 0,
+		var date = new Date();
+		newMessage.set({'userName' : $('#userNameValue').val(), 'password' : $('#passwordValue').val(), 'date' : date.getFullYear() + " " + date.getMonth() + " " + date.getDate(),
         'location' : locationString, 'isMale' : true, 'content' : $('#contentValue').val(),
         'email' : $('#emailValue').val(), 'phone' : $('#phoneValue').val(), 'qq' : $('#qqValue').val(), 'selfDefined' : $('#selfDefinedValue').val(),
         'price' : Number($('#priceValue').val()), 'type' : 0});
